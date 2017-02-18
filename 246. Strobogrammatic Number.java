a0ea47e4e80694dd901c3a50c8aba3d40448aa8a
Di Wang <jiankong3@gmail.com>
2017-02-18 15:21:16 -0500

    Hashtable

---
+++ 246. Strobogrammatic Number.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public boolean isStrobogrammatic(String num) {
+        
+        HashMap <Character,Character> dic = new HashMap <Character,Character> ();
+        
+        dic.put('8','8');
+        dic.put('6','9');
+        dic.put('9','6');
+        dic.put('1','1');
+        dic.put('0','0');
+        
+        int i = 0;
+        int j = num.length()-1;
+        
+        while(i<=j){
+            if (!dic.containsKey(num.charAt(i))){
+                return false;
+            } 
+            
+            if (dic.get(num.charAt(i))!=num.charAt(j)){
+                return false;
+            }
+            
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+}
