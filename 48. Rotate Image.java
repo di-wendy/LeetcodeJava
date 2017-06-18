dbd6658283d0be908fdbecf978c575db052e03eb
Di Wang <jiankong3@gmail.com>
2017-06-17 21:19:20 -0400

    Array

---
+++ 48. Rotate Image.java
@@ -0,0 +1,26 @@
+//Flip, the loop length is .length/2 instead of .length
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        
+        //i row number, j column number
+        
+        for(int i = 0; i < matrix.length - 1; i++){
+            for(int j = i + 1; j < matrix.length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix.length/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+        
+        return;
+    }
+}
