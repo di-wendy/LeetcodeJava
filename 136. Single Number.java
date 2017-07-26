e2c2be540ad0f899fa23dca3da680105b4e1f56f
Di Wang <jiankong3@gmail.com>
2017-07-25 22:36:03 -0400

    Create  136. Single Number.java

---
+++ 136. Single Number.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int ans = 0;
+        
+        for(int n:nums){
+            ans ^= n;
+        }
+        
+        return ans;
+    }
+}
