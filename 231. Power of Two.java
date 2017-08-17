58f92f253604292e80f6e26d545db68b0afe371a
Di Wang <jiankong3@gmail.com>
2017-08-17 09:59:12 -0400

    Bit Manu

--- 231. Power of Two.java
+++ 231. Power of Two.java
@@ -3,3 +3,14 @@ public class Solution {
         return n>0 && Integer.bitCount(n) == 1;
     }
 }
+//
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++){
+            ans += (n >> i & 1);
+        }
+        return ans;
+    }
+}
