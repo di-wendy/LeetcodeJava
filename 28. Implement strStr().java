2ceedba54e3b22cd45e3540fd6c8d0698c399afe
Di Wang <jiankong3@gmail.com>
2017-08-26 11:52:13 -0400

    Two Pointer

--- 28. Implement strStr().java
+++ 28. Implement strStr().java
@@ -1,56 +1,21 @@
-//Own Method beat 58%
-public class Solution {
-    public int strStr(String haystack, String needle) {
-        
-        int str1 = haystack.length();
-        int str2 = needle.length();
-        
-        if(str2 == 0){
-            return 0;
-        }
-        if(str1 == 0){
-            return -1;
-        }
-        
-        //Two pointers
-        int i = 0;
-        int j = 0;
-        
-        while(i < str1-str2+1){
-            if(haystack.charAt(i) != needle.charAt(0)){
-                i++;
-                j++;
-            }
-            else{
-                while(j-i <= str2){
-                    if(haystack.charAt(j) == needle.charAt(j-i)){
-                        j++;
-                        
-                        if(j-i == str2){
-                            return i;
-                        }
-                    }
-                    else{
-                        i += 1;
-                        j = i;
-                        break;
-                    }
+class Solution {
+    public int strStr(String source, String target) {
+        if (source == null || target == null) {
+                    return -1;
                 }
-                
+
+        for (int i = 0; i < source.length() - target.length() + 1; i++) {
+            int j = 0;
+            for (j = 0; j < target.length(); j++) {
+                if (source.charAt(i + j) != target.charAt(j)) {
+                    break;
+                }
+            }
+            // finished loop, target found
+            if (j == target.length()) {
+                return i;
             }
         }
-        
-        return -1;
+            return -1;
     }
-}
-
-//Elegant Java
-public int strStr(String haystack, String needle) {
-  for (int i = 0; ; i++) {
-    for (int j = 0; ; j++) {
-      if (j == needle.length()) return i;
-      if (i + j == haystack.length()) return -1;
-      if (needle.charAt(j) != haystack.charAt(i + j)) break;
-    }
-  }
 }
