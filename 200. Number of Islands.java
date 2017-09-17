8d830c8824624a4ee109692153abb8057e7a2833
Di Wang <jiankong3@gmail.com>
2017-09-17 17:09:02 -0400

    Update 200. Number of Islands.java

--- 200. Number of Islands.java
+++ 200. Number of Islands.java
@@ -36,3 +36,41 @@ public class Solution {
         return;
     }
 }
+
+//Second Solution
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) return 0;
+        Stack<Integer> x = new Stack<>();
+        Stack<Integer> y = new Stack<>();
+        int count = 0;
+        int[] dx = new int[]{-1,0,0,1};
+        int[] dy = new int[]{0,1,-1,0};
+        
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[0].length; j++){
+                if (grid[i][j] == '1'){
+                    count ++;
+                    x.push(i);
+                    y.push(j);
+                    grid[i][j] = '0';
+                }
+                while (!x.isEmpty()){
+                    int px = x.pop();
+                    int py = y.pop();
+                    for (int k = 0; k < 4; k++){
+                        if (px + dx[k] >= 0 && px + dx[k] < grid.length
+                        && py + dy[k] >= 0 && py + dy[k] < grid[0].length){
+                            if (grid[px + dx[k]][py + dy[k]] == '1'){
+                                x.push(px + dx[k]);
+                                y.push(py + dy[k]);
+                                grid[px + dx[k]][py + dy[k]] = '0';
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
