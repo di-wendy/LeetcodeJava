f4507a39130066bf5bfed72ad86fddaa44fc960c
Di Wang <jiankong3@gmail.com>
2017-09-17 18:17:29 -0400

    DP[][]

--- 5. Longest Palindromic Substring.java
+++ 5. Longest Palindromic Substring.java
@@ -2,8 +2,9 @@ class Solution {
     public String longestPalindrome(String s) {
         String res = "";
         boolean[][] dp = new boolean[s.length()][s.length()];
-        for (int i = 0; i < s.length(); i++){
-            for (int j = i; j < s.length(); j++){
+        //Update Column first
+        for (int j = 0; j < s.length(); j++){
+            for (int i = j; i >= 0; i--){
                 if (s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i+1][j-1])){
                     dp[i][j] = true;
                 }
