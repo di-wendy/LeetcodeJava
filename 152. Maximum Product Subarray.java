e3170070527a05ab6f67f3872c0f353eee044ad0
Di Wang <jiankong3@gmail.com>
2017-09-23 10:54:07 -0400

    DP Product

--- 152. Maximum Product Subarray.java
+++ 152. Maximum Product Subarray.java
@@ -1,3 +1,4 @@
+//
 public class Solution {
     public int maxProduct(int[] nums) {
         
@@ -23,6 +24,35 @@ public class Solution {
             ans = Math.max(ans,n);
         }
 
+        return ans;
+    }
+}
+//Two Array
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+        int[] pos = new int[nums.length];
+        int[] neg = new int[nums.length];
+        
+        pos[0] = nums[0];
+        neg[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] > 0){
+                pos[i] = Math.max(nums[i], pos[i - 1] * nums[i]);
+                neg[i] = Math.min(nums[i], neg[i - 1] * nums[i]);
+            }
+            if (nums[i] < 0){
+                pos[i] = Math.max(nums[i], neg[i - 1] * nums[i]);
+                neg[i] = Math.min(nums[i], pos[i - 1] * nums[i]);
+            }
+        }
+        
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++){
+            ans = Math.max(ans, pos[i]);
+            ans = Math.max(ans, neg[i]);
+        }
         return ans;
     }
 }
