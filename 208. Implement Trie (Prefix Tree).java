9463d08b9eec5ca7a6d0fd45fcfd1e59dd1e75be
Di Wang <jiankong3@gmail.com>
2018-04-25 08:21:37 -0700

    Trie

--- 208. Implement Trie (Prefix Tree).java
+++ 208. Implement Trie (Prefix Tree).java
@@ -1,34 +1,11 @@
+class TrieNode {
+    char c;
+    HashMap<Character, TrieNode> children = new HashMap<>();
+    boolean hasWord;
+}
+
+
 public class Trie {
-    
-    public class TrieNode{
-        private TrieNode[] children;
-        public boolean EndNode;
-
-        public TrieNode(){
-            this.children = new TrieNode[26];
-            this.EndNode = false;
-        }
-
-        public void insert(String word, int index){
-            if(index == word.length()){
-                EndNode = true;
-                return;
-            }
-            int temp = word.charAt(index) - 'a';
-            if(children[temp] == null)
-                children[temp] = new TrieNode();
-            children[temp].insert(word, index + 1);
-        }
-
-        public TrieNode find(String word, int index){
-            if(index == word.length())
-                return this;
-            int temp = word.charAt(index) - 'a';
-            if(children[temp] == null)
-                return null;
-            return children[temp].find(word, index + 1);
-        }
-    }
     private TrieNode root;
     /** Initialize your data structure here. */
     public Trie() {
@@ -37,18 +14,48 @@ public class Trie {
     
     /** Inserts a word into the trie. */
     public void insert(String word) {
-        root.insert(word, 0);
+        TrieNode cur = root;
+        HashMap<Character, TrieNode> curChildren = root.children;
+        
+        for (char c: word.toCharArray()){
+            if (!curChildren.containsKey(c)){
+                curChildren.put(c, new TrieNode());
+            }
+            cur = curChildren.get(c);
+            curChildren = cur.children;
+        }
+        cur.hasWord = true;
     }
     
     /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        TrieNode temp = root.find(word, 0);
-        return (temp != null && temp.EndNode);
+        TrieNode finalNode = searchNode(word);
+        return (finalNode != null && finalNode.hasWord);
     }
     
     /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-        TrieNode temp = root.find(prefix, 0);
-        return (temp != null);
+        return (searchNode(prefix) != null);
+    }
+    
+    public TrieNode searchNode(String word){
+        TrieNode cur = root;
+        HashMap<Character, TrieNode> curChildren = root.children;
+        for (char c : word.toCharArray()){
+            if (!curChildren.containsKey(c)){
+                return null;
+            }
+            cur = curChildren.get(c);
+            curChildren = cur.children;
+        }
+        return cur;
     }
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
