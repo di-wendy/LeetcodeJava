ec75c1d6440d46f6c23fc5978de0976eea00a79c
Di Wang <jiankong3@gmail.com>
2018-05-09 20:33:45 -0700

    Update 34. Search for a Range.java

--- 34. Search for a Range.java
+++ 34. Search for a Range.java
@@ -38,3 +38,51 @@ public class Solution {
         return ans;
     }
 }
+
+//
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = new int[]{-1,-1};
+        
+        if (nums.length < 1){
+            return ans;
+        }
+        
+        int l = 0;
+        int r = nums.length - 1;
+        
+        while (l + 1 < r){
+            int mid = (l + r) / 2;
+            if (nums[mid] == target){
+                l = mid;
+                r = mid;
+                break;
+            }
+            else if (nums[mid] < target){
+                l = mid;
+            }
+            else{
+                r = mid;
+            }
+        }
+        
+        if (nums[l] != target && nums[r] != target){
+            return ans;
+        }
+        
+        int p = (nums[l] == target) ? l : r;
+        int q = (nums[r] == target) ? r : l;
+        
+        while (p >= 0 && nums[p] == target){
+            p--;
+        }
+        ans[0] = p + 1;
+        
+        while (q < nums.length && nums[q] == target){
+            q++;
+        }
+        ans[1] = q - 1;
+        
+        return ans;
+    }
+}
