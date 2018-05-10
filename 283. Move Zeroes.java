be436d9a6597a760a3088d2021e1cd8e249f1d67
Di Wang <jiankong3@gmail.com>
2018-05-10 07:34:30 -0700

    Update 283. Move Zeroes.java

--- 283. Move Zeroes.java
+++ 283. Move Zeroes.java
@@ -14,3 +14,33 @@ class Solution {
         
     }
 }
+
+//Swap
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int p = 0; //point to zero
+        int q = 0; //the first non zero from p
+        
+        while (p < nums.length && q < nums.length){
+            if (nums[p] == 0){
+                while (q < nums.length && nums[q] == 0){
+                    q ++;
+                }
+                if (q == nums.length){
+                    break;
+                }
+                swap(nums, p, q);
+            }
+            p++;
+            q++;
+        }
+        
+        return;
+    }
+    
+    public void swap(int[] nums, int p, int q){
+        int temp = nums[p];
+        nums[p] = nums[q];
+        nums[q] = temp;
+    }
+}
