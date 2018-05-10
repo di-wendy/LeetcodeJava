7f5f91d8cdb4608c9fbdf3db9af61b622d7751bd
Di Wang <jiankong3@gmail.com>
2018-05-10 08:12:53 -0700

    Update 47. Permutations II.java

--- 47. Permutations II.java
+++ 47. Permutations II.java
@@ -33,3 +33,40 @@ public class Solution {
         }
     }
 }
+
+//Easier to understand
+class Solution {
+    //Key: For repeated element, if not selected the first one, 
+    //will not select the rest
+    public List<List<Integer>> permuteUnique(int[] nums) {
+       List<List<Integer>> ans = new ArrayList<>();
+            Arrays.sort(nums);
+            helper(ans, new ArrayList<Integer>(), new boolean[nums.length], nums);
+            return ans;
+    }
+    
+    public void helper(List<List<Integer>> ans,
+                      List<Integer> temp,
+                      boolean[] visited,
+                      int[] nums){
+        if (temp.size() == nums.length){
+            ans.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++){
+            if (!visited[i]){
+                if (i > 0 && nums[i - 1] == nums[i]){
+                    if (!visited[i - 1]){
+                        continue;
+                    }
+                }
+                visited[i] = true;
+                temp.add(nums[i]);
+                helper(ans, temp, visited, nums);
+                temp.remove(temp.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
