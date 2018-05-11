e79cb52bf1058c6197efb1f8a06d06e00c28eb44
Di Wang <jiankong3@gmail.com>
2018-05-10 22:30:58 -0700

    Update 55. Jump Game.java

--- 55. Jump Game.java
+++ 55. Jump Game.java
@@ -1,21 +1,16 @@
-public class Solution {
+class Solution {
     public boolean canJump(int[] nums) {
+        boolean[] reach = new boolean[nums.length];
+        reach[0] = true;
         
-        if(nums.length < 1) return true;
-        
-        boolean[] store = new boolean[nums.length];
-        store[0] = true;
-        
-        for(int i = 1; i < nums.length; i++){
-            store[i] = false;
-            for(int j = i - 1; j >= 0; j --){
-                if(store[j] == true && (i - j) <= nums[j]){
-                    store[i] = true;
+        for (int i = 1; i < nums.length; i++){
+            for (int j = i - 1; j >= 0; j--){
+                if (reach[j] && (i - j) <= nums[j]){
+                    reach[i] = true;
                     break;
                 }
             }
         }
-        
-        return store[nums.length - 1];
+        return reach[nums.length - 1];
     }
 }
