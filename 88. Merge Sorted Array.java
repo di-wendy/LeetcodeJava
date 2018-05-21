fe61e2293ad90dced10dd47d07b82d78045256b3
Di Wang <jiankong3@gmail.com>
2018-05-20 18:19:47 -0700

    O(n)

--- 88. Merge Sorted Array.java
+++ 88. Merge Sorted Array.java
@@ -1,12 +1,10 @@
-public class Solution {
+class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
-        //Merge Sort
-        int k = m + n -1;
-        m-=1;
-        n-=1;
-        while(m>=0 && n>=0) nums1[k--] = (nums1[m]>nums2[n]) ? nums1[m--] : nums2[n--];
-        while(n>=0) nums1[k--] = nums2[n--];
-        
+        int i = m - 1;
+        int j = n - 1;
+        int pos = m + n - 1;
+        while (j >= 0){
+            nums1[pos--] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--]:nums2[j--];
+        }
     }
 }
