416455b671480ba91c721fb5104c502c4485a32f
Di Wang <jiankong3@gmail.com>
2018-05-23 08:10:03 -0700

    Update 111. Minimum Depth of Binary Tree.java

--- 111. Minimum Depth of Binary Tree.java
+++ 111. Minimum Depth of Binary Tree.java
@@ -25,3 +25,12 @@ class Solution {
         if (root.right != null) search(root.right, depth + 1);
     }
 }
+//Easier Method
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
+    }
+}
