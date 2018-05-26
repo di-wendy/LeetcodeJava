eac7391baf4433c28e1064d81dc05d9a33cb54b6
Di Wang <jiankong3@gmail.com>
2018-05-26 16:30:43 -0700

    Divide and conquer

--- 437. Path Sum III.java
+++ 437. Path Sum III.java
@@ -7,15 +7,15 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
+class Solution {
     public int pathSum(TreeNode root, int sum) {
-        if(root == null) return 0;
-        return calSum(root,sum) + pathSum(root.left,sum) + pathSum(root.right,sum);
+        if (root == null) return 0;
+        return startFromMe(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
     }
     
-    public int calSum(TreeNode root, int sum){
-        if(root == null) return 0;
-        int add = (root.val == sum)?1:0;
-        return calSum(root.left,sum-root.val) + add + calSum(root.right,sum-root.val);
+    public int startFromMe(TreeNode root, int sum){
+        if (root == null) return 0;
+        int cur = (root.val == sum) ? 1 : 0;
+        return startFromMe(root.left, sum - root.val) + cur + startFromMe(root.right, sum - root.val);
     }
 }
