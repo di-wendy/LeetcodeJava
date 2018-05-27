479b4bf830831773ada3b56dcdd6a030d49bdde1
Di Wang <jiankong3@gmail.com>
2018-05-26 17:47:55 -0700

    Two Pointers

--- 42. Trapping Rain Water.java
+++ 42. Trapping Rain Water.java
@@ -1,31 +1,31 @@
-public class Solution {
+class Solution {
     public int trap(int[] height) {
+        if (height == null || height.length <= 1) return 0;
         
-        if(height.length < 3) return 0;
+        int leftMin = height[0];
+        int rightMin = height[height.length - 1];
+        int l = 0;
+        int r = height.length - 1;
+        int total = 0;
         
-        //Two pointer
-        int ans = 0;
-        int left = 0;
-        int right = height.length - 1;
-        
-        while(left < height.length - 1 && height[left] < height[left + 1]) left++;
-        while(right > 0 && height[right] < height[right - 1]) right --;
-        
-        while(left < right){
-            int lmax = height[left];
-            int rmax = height[right];
-            if(lmax < rmax){
-                while(left <= right && height[++left] < lmax){
-                    ans += lmax - height[left];
+        while (l <= r) { //l and r are not processed
+            if (leftMin < rightMin){
+                if (leftMin > height[l]){
+                    total += leftMin - height[l];
+                } else {
+                    leftMin = height[l];
                 }
-            }
-            else{
-                while(left <= right && height[--right] < rmax){
-                    ans += rmax - height[right];
+                l++;
+            } else {
+                if (rightMin > height[r]){
+                    total += rightMin - height[r];
+                } else {
+                    rightMin = height[r];
                 }
+                r--;
             }
         }
-       
-        return ans;
+        
+        return total;
     }
 }
