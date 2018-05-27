e657e5c777f912c8544f24c154ecdfd1ad6e90ed
Di Wang <jiankong3@gmail.com>
2018-05-27 15:51:02 -0700

    Recursion

--- 617. Merge Two Binary Trees.java
+++ 617. Merge Two Binary Trees.java
@@ -7,34 +7,17 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
+class Solution {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        
-        if (t1 == null && t2 == null) return null;
-        
-        int v1 = 0;
-        int v2 = 0;
-        
-        if(t1 != null) v1 = t1.val;
-        if(t2 != null) v2 = t2.val;
-
-        TreeNode ansnode = new TreeNode(v1 + v2);
-        
-        if(t1 == null) {
-            ansnode.left = mergeTrees(null,t2.left);
-            ansnode.right = mergeTrees(null,t2.right);
+        if (t1 != null && t2 != null){
+            t1.val = t1.val + t2.val;
+            t1.left = mergeTrees(t1.left, t2.left);
+            t1.right = mergeTrees(t1.right, t2.right);
+            return t1;
+        } else if (t1 == null){
+            return t2;
+        } else {
+            return t1;
         }
-        else if(t2 == null){
-            ansnode.left = mergeTrees(t1.left,null);
-            ansnode.right = mergeTrees(t1.right,null);
-        }
-        
-        else{
-            ansnode.left = mergeTrees(t1.left,t2.left);
-            ansnode.right = mergeTrees(t1.right,t2.right);
-        }
-        
-        return ansnode;
     }
-
 }
