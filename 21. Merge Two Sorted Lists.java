c3c19330c25a07536035244e62f658031baa154a
Di Wang <jiankong3@gmail.com>
2018-05-28 09:21:59 -0700

    Linked list recursion and iterative

--- 21. Merge Two Sorted Lists.java
+++ 21. Merge Two Sorted Lists.java
@@ -25,3 +25,26 @@ public class Solution {
         return mergehead;
     }
 }
+
+//Iterative
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val){
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 != null) cur.next = l1;
+        if (l2 != null) cur.next = l2;
+        
+        return dummy.next;
+    }
+}
