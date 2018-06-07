e000136baf2eed774c18977a3d631bc0dd12261b
Di Wang <jiankong3@gmail.com>
2018-06-06 17:19:58 -0700

    DP

--- 198. House Robber.java
+++ 198. House Robber.java
@@ -12,3 +12,21 @@ public class Solution {
         return val;
     }
 }
+
+//Easier to understand
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        
+        //init
+        int notRob = 0;
+        int Robbed = nums[0];
+        //update
+        for (int i = 1; i < nums.length; i++){
+            int temp = notRob;
+            notRob = Math.max(notRob, Robbed); //not rob current, can take either pre
+            Robbed = temp + nums[i]; //rob current, can only take not rob pre
+        }
+        return Math.max(notRob, Robbed);
+    }
+}
