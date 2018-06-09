b993dcd929c4ae4b1852214bceb14862868e42cf
Di Wang <jiankong3@gmail.com>
2018-06-08 17:29:37 -0700

    Java

--- 139. Word Break.java
+++ 139. Word Break.java
@@ -1,20 +1,26 @@
-public class Solution {
+class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        
-        //Dynamic Programming
-        boolean[] store = new boolean[s.length() + 1];
-        
-        store[0] = true;
-        
-        for(int i = 1; i <= s.length(); i++){
-            for(int j = 0; j < i; j++){
-                if(store[j] && wordDict.contains(s.substring(j,i))){
-                    store[i] = true;
+        int maxlen = 0;
+        int minlen = Integer.MAX_VALUE;
+        Set<String> dict = new HashSet<String>();
+        for (int i = 0; i < wordDict.size(); i++){
+            dict.add(wordDict.get(i));
+            maxlen = Math.max(maxlen, wordDict.get(i).length());
+            minlen = Math.min(minlen, wordDict.get(i).length());
+        }
+        boolean[] test = new boolean[s.length() + 1];
+        //...ith letter is a word break or not
+        test[0] = true;
+        for (int i = 1; i <= s.length(); i++){
+            int len = minlen;
+            while (i - len >= 0 && len <= maxlen && test[i] == false){
+                if (test[i - len] && dict.contains(s.substring(i - len, i))){
+                    test[i] = true;
+                } else {
+                    len++;
                 }
             }
         }
-        
-        return store[store.length - 1];
-        
+        return test[s.length()];
     }
 }
