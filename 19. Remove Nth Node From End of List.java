fb61c33777679f8328aaa29d4d6cf31684fb19c5
Di Wang <jiankong3@gmail.com>
2018-06-11 23:51:53 -0700

    Linked List

--- 19. Remove Nth Node From End of List.java
+++ 19. Remove Nth Node From End of List.java
@@ -31,3 +31,25 @@ class Solution {
         return dummy.next;
     }
 }
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        
+        int count = 0;
+        while (first != null){
+            first = first.next;
+            if (count > n) second = second.next;
+            count += 1;
+        }
+        //diff is n + 1
+        if (second.next == null) second = null;
+        else second.next = second.next.next;
+
+        return dummy.next;
+    }
+}
