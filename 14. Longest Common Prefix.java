6cfd336e83f1dbd10d7938951e970be0c1f1ed3b
Di Wang <jiankong3@gmail.com>
2018-06-14 19:41:48 -0700

    Update

--- 14. Longest Common Prefix.java
+++ 14. Longest Common Prefix.java
@@ -1,13 +1,9 @@
 //94.8%
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        
         if(strs.length == 0) return "";
-        
         String ans = strs[0];
-        
         int i = 1;
-        
         while(i < strs.length){
             while(strs[i].indexOf(ans)!=0){
                 ans = ans.substring(0,ans.length()-1);
