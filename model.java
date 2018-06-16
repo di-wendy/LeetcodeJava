e59c1695c8ceff7646c6d02b3ccd1fdfe0d18284
Di Wang <wangdi561@Dis-MacBook-Pro.local>
2018-06-16 09:16:30 -0700

    local change

--- model.java
+++ model.java
@@ -2,9 +2,26 @@ import java.util.*;
 
 class model{
 
+    public static List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(k, n, 1, ans, 0, new ArrayList<Integer>());
+        return ans;
+    }
+
+    public static void helper(int k, int n, int cur, List<List<Integer>> ans, int sum, List<Integer> temp){
+        if (temp.size() == k){
+            if (n == sum) ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = cur; i <= k; i++){
+            temp.add(i);
+            helper(k, n, i + 1, ans, sum + i, temp);
+            temp.remove(temp.size() - 1);
+            helper(k, n, i + 1, ans, sum, temp);
+        }
+    }
 
     public static void main(String[] args){
-
-        System.out.print();
+        combinationSum3(3, 7);
     }
 }
