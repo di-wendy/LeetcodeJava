0846ae23a1bb60e3ed2503b760fe4483973ad5e6
Di Wang <jiankong3@gmail.com>
2018-06-16 12:44:18 -0700

    Update 163. Missing Ranges.java

--- 163. Missing Ranges.java
+++ 163. Missing Ranges.java
@@ -18,3 +18,30 @@ public class Solution {
         return ans;
     }
 }
+
+//Another method
+class Solution {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> ans = new ArrayList<>();
+        if (nums.length == 0) {
+            ans.add(convertRange(lower, upper));
+            return ans;
+        }
+        if (lower < nums[0]) ans.add(convertRange(lower, nums[0] - 1));
+        for (int i = 0; i < nums.length - 1; i++){
+            long diff = (long)nums[i + 1] - (long)nums[i];
+            if (diff <= 1) continue;
+            ans.add(convertRange(nums[i] + 1, nums[i + 1] - 1));
+        }
+        if (nums[nums.length - 1] < upper){
+            ans.add(convertRange(nums[nums.length - 1] + 1, upper));
+        }
+        return ans;
+    }
+    
+    public String convertRange(int i, int j){
+        //inclusive range
+        if (i == j) return Integer.toString(i);
+        return Integer.toString(i) + "->" + Integer.toString(j);
+    }
+}
