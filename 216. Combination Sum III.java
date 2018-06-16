0b8f858fce752c596fc5f480bdd85f805b01d23b
Di Wang <jiankong3@gmail.com>
2018-06-16 09:29:06 -0700

    BackTracking

---
+++ 216. Combination Sum III.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(k, n, 1, ans, 0, new ArrayList<Integer>());
+        return ans;
+    }
+    
+    public void helper(int k, int n, int cur, List<List<Integer>> ans, int sum, List<Integer> temp){
+        if (temp.size() == k){
+            if (n == sum) ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = cur; i <= 9; i++){
+            temp.add(i);
+            helper(k, n, i + 1, ans, sum + i, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
