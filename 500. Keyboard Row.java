edb204fdbeeca42e85274ce2e6d7bb3b329fe8c6
Di Wang <jiankong3@gmail.com>
2018-06-23 14:10:27 -0700

    Update 500. Keyboard Row.java

--- 500. Keyboard Row.java
+++ 500. Keyboard Row.java
@@ -1,33 +1,25 @@
-public class Solution {
+class Solution {
     public String[] findWords(String[] words) {
-
-        List<String> answer = new ArrayList<String>();
-        
-        Set<Character> h1 = new HashSet<Character>(Arrays.asList('Q','W','E','R','T','Y','U','I','O','P'));
-        Set<Character> h2 = new HashSet<Character>(Arrays.asList('A','S','D','F','G','H','J','K','L'));
-        Set<Character> h3 = new HashSet<Character>(Arrays.asList('Z','X','C','V','B','N','M'));
-        Set<Character> pointer = new HashSet<Character>();
-        
-        for(String str:words){
-            if (str.length()<=1) 
-            {answer.add(str);
-             continue;}
-            String up_str = str.toUpperCase();
-            if (h1.contains(up_str.charAt(0))){pointer = h1;}
-            if (h2.contains(up_str.charAt(0))){pointer = h2;}
-            if (h3.contains(up_str.charAt(0))){pointer = h3;}
-            
-            boolean in_a_line = true;
-            for(int i=1; i<up_str.length(); i++){
-                if (!pointer.contains(up_str.charAt(i))) {
-                    in_a_line = false;
-                    break;
-                }
+        String[] keys = new String[]{"QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"};
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < keys.length; i++){
+            for (char c : keys[i].toCharArray()){
+                map.put(c, i);
             }
-            if(in_a_line){answer.add(str);}
         }
-        
-        String[] y = answer.toArray(new String[0]);
-        return y;
+        List<String> ans = new ArrayList<>();
+        for (String s : words){
+            if (s.length() <= 1) {ans.add(s); continue;} //KEY
+            int cur = map.get(Character.toUpperCase(s.charAt(0)));
+            for (int i = 1; i < s.length(); i++){
+                if (map.get(Character.toUpperCase(s.charAt(i))) != cur) break;
+                if (i == s.length() - 1) ans.add(s);
+            }
+        }
+        String[] output = new String[ans.size()];
+        for (int i = 0; i < output.length; i++){
+            output[i] = ans.get(i);
+        }
+        return output;
     }
 }
