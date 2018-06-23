7debb4bc66127411362f5a04e193586c79df74ad
Di Wang <jiankong3@gmail.com>
2018-06-23 14:47:44 -0700

    No HashMap Update

--- 208. Implement Trie (Prefix Tree).java
+++ 208. Implement Trie (Prefix Tree).java
@@ -59,3 +59,53 @@ public class Trie {
  * boolean param_2 = obj.search(word);
  * boolean param_3 = obj.startsWith(prefix);
  */
+
+//No HashMap
+class Node{
+    Node[] children;
+    boolean isEnd;
+    public Node(){
+        children = new Node[26];
+        isEnd = false;
+    }
+}
+
+public class Trie {
+    Node head;
+    /** Initialize your data structure here. */
+    public Trie() {
+        head = new Node();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        Node cur = head;
+        for (char c : word.toCharArray()){
+            if (cur.children[c - 'a'] == null){
+                cur.children[c - 'a'] = new Node();
+            }
+            cur = cur.children[c - 'a'];
+        }
+        cur.isEnd = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        Node cur = head;
+        for (char c : word.toCharArray()){
+            if (cur.children[c - 'a'] == null) return false;
+            cur = cur.children[c - 'a'];
+        }
+        return cur.isEnd;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        Node cur = head;
+        for (char c : prefix.toCharArray()){
+            if (cur.children[c - 'a'] == null) return false;
+            cur = cur.children[c - 'a'];
+        }
+        return true;
+    }
+}
