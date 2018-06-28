32328af653c0633c8612d4887a4e49ac44f4fc5e
Di Wang <jiankong3@gmail.com>
2018-06-28 08:39:36 -0700

    Topo Sort

--- 210. Course Schedule II.java
+++ 210. Course Schedule II.java
@@ -1,48 +1,44 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] req = new int[numCourses];
-        Map<Integer,List<Integer>> mp = new HashMap<>();
-        
-        for(int[] course:prerequisites){
-            if(!mp.containsKey(course[1])){
-                mp.put(course[1],new ArrayList<Integer>());
-                mp.get(course[1]).add(course[0]);
-            }
-            else{
-                mp.get(course[1]).add(course[0]);
-            }
-            req[course[0]] ++;
-        }
-        
-        int count = 0;
-        int[] res = new int[numCourses];
+        //拓扑排序，关系表示依赖
+        List[] graph = new ArrayList[numCourses];
+        int[] degree = new int[numCourses];
         Queue<Integer> q = new LinkedList();
+        List<Integer> courses = new ArrayList<>();
         
-        for (int i = 0; i < numCourses; i++){
-            if(req[i] == 0){
-                res[count] = i;
-                count ++;
-                q.add(i);
+        for(int i=0; i<numCourses; i++) graph[i] = new ArrayList<Integer>();
+        
+        for (int i = 0; i < prerequisites.length; i++){
+            degree[prerequisites[i][1]]++; //outDegree
+            graph[prerequisites[i][0]].add(prerequisites[i][1]);
+        }
+        
+        for(int i = 0; i < degree.length;i++){
+            if(degree[i] == 0){
+                q.offer(i);
+                courses.add(i);
             }
         }
         
-        while(!q.isEmpty()){
-            int course = q.poll();
-            if (!mp.containsKey(course)) continue;
-            for(int n : mp.get(course)){
-                req[n] --;
-                if (req[n] == 0){
-                    if(count < numCourses){
-                        res[count] = n;
-                        count ++;
-                        q.add(n);
+        while (!q.isEmpty()){
+            int size = q.size();
+            for (int i = 0; i < size; i++){
+                int cur = (int)q.poll();
+                for (int n = 0; n < graph[cur].size(); n++){
+                    int num = (int)graph[cur].get(n);
+                    degree[num]--;
+                    if(degree[num] == 0) {
+                        q.offer(num);
+                        courses.add(num);
                     }
                 }
             }
         }
-        
-        if(count != numCourses) return new int[0];
-        
-        return res;
+        if (courses.size() < numCourses) return new int[0];
+        int[] ans = new int[courses.size()];
+        for (int i = courses.size() - 1; i >= 0; i--){
+            ans[courses.size() - 1 - i] = courses.get(i);
+        }
+        return ans;
     }
 }
