031d5f030933be062d19b923f06f8c6e3a8f54be
Di Wang <jiankong3@gmail.com>
2018-06-28 08:29:31 -0700

    Update
    The Array.fill(array, new ArrayList<>) can cause problems

--- 207. Course Schedule.java
+++ 207. Course Schedule.java
@@ -1,36 +1,40 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList[] graph = new ArrayList[numCourses];
+        List[] graph = new ArrayList[numCourses];
         int[] degree = new int[numCourses];
-        Queue queue = new LinkedList();
-        int count=0;
+        Queue<Integer> q = new LinkedList();
+        int count = 0;
         
-        for(int i=0;i<numCourses;i++)
-            graph[i] = new ArrayList();
+        for(int i=0;i<numCourses;i++) graph[i] = new ArrayList();
         
-        
-        for(int i=0; i<prerequisites.length;i++){
-            degree[prerequisites[i][1]]++; //required
+        for (int i = 0; i < prerequisites.length; i++){
+            degree[prerequisites[i][1]]++; //indegree
             graph[prerequisites[i][0]].add(prerequisites[i][1]);
         }
-        for(int i=0; i<degree.length;i++){
+        
+        for(int i = 0; i < degree.length;i++){
             if(degree[i] == 0){
-                queue.add(i);
+                q.offer(i);
+                System.out.print(i);
                 count++;
             }
         }
         
-        while(queue.size() != 0){
-            int course = (int)queue.poll();
-            for(int i=0; i<graph[course].size();i++){
-                int pointer = (int)graph[course].get(i);
-                degree[pointer]--;
-                if(degree[pointer] == 0){
-                    queue.add(pointer);
-                    count++;
+        while (!q.isEmpty()){
+            int size = q.size();
+            for (int i = 0; i < size; i++){
+                int cur = (int)q.poll();
+                for (int n = 0; n < graph[cur].size(); n++){
+                    int num = (int)graph[cur].get(n);
+                    degree[num]--;
+                    if(degree[num] == 0) {
+                        q.offer(num);
+                        count++;
+                    }
                 }
             }
         }
+        
         return count == numCourses;
     }
 }
