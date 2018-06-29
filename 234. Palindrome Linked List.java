a1b16514978a803875101bba2b7d19311dbf15f7
Di Wang <jiankong3@gmail.com>
2018-06-29 07:39:02 -0700

    Lined list

--- 234. Palindrome Linked List.java
+++ 234. Palindrome Linked List.java
@@ -6,6 +6,44 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+//
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        //Find the middle node and reverse it
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode preslow = null;
+        while (fast != null && fast.next != null){
+            preslow = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (preslow != null) preslow.next = null;
+        
+        ListNode middle = reverseLinkedList(slow);
+        ListNode c = middle;
+        
+        while (head != null && middle != null){
+            if (head.val != middle.val) return false;
+            head = head.next;
+            middle = middle.next;
+        }
+        return true;
+    }
+    
+    public ListNode reverseLinkedList(ListNode Node){
+        if (Node == null || Node.next == null) return Node;
+        ListNode pre = null;
+        while (Node != null){
+            ListNode future = Node.next;
+            Node.next = pre;
+            pre = Node;
+            Node = future;
+        }
+        return pre;
+    }
+}
+//
 public class Solution {
     public boolean isPalindrome(ListNode head) {
         
