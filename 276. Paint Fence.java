dac34f54a9172509128fcfa778685d6f98ab954d
Di Wang <jiankong3@gmail.com>
2018-06-29 17:25:43 -0700

    Update

--- 276. Paint Fence.java
+++ 276. Paint Fence.java
@@ -1,25 +1,14 @@
-public class Solution {
+class Solution {
     public int numWays(int n, int k) {
-        int a0 = k;
-        int a1 = (int)Math.pow((double)k,(double)2);
-        int a2 = a1*k - a0;
-        int ans = 0;
-
-        //Initialization
-        if (n==0) return 0;
-        if (n==1) return a0;
-        if (n==2) return a1;
-        if (n==3) return a2;
-        
-        for(int i=4; i<=n; i++){
-
-            ans = a2*k - a0*(k-1);
-
-            a0 = a1;
-            a1 = a2;
-            a2 = ans;
+        if (n == 0) return 0;
+        if (n == 1) return k;
+        int diff = k*(k - 1);
+        int same = k;
+        for (int i = 2; i < n; i++){
+            int temp = diff;
+            diff = (diff + same) * (k - 1);
+            same = temp;
         }
-        
-        return ans;
+        return diff + same;
     }
 }
