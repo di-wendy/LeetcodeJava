8e03129e951d98b0baab4d575968f854f533eae5
Di Wang <jiankong3@gmail.com>
2018-07-03 07:45:35 -0700

    Update

--- 137. Single Number II.java
+++ 137. Single Number II.java
@@ -2,18 +2,27 @@ public class Solution {
     public int singleNumber(int[] nums) {
         
         int res = 0;
-        
         int[] bits = new int[32];
         
         for(int i = 0; i < 32; i++){
-            for(int n:nums){
+            for(int n : nums){
                 bits[i] += (n >> i) & 1;
                 bits[i] %= 3;
             }
-            
             res |= bits[i] << i;
         }
-        
         return res;
+    }
+}
+//Update
+class Solution {
+    public int singleNumber(int[] nums) {
+        int a = 0;
+        int b = 0;
+        for (int n : nums){
+            b = (b ^ n) & ~a;
+            a = (a ^ n) & ~b;
+        }
+        return b;
     }
 }
