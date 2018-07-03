8471a10725b2ec876ec0cb09ce8adecf01806758
Di Wang <jiankong3@gmail.com>
2018-07-03 08:21:31 -0700

    Binary Search

--- 540. Single Element in a Sorted Array.java
+++ 540. Single Element in a Sorted Array.java
@@ -2,6 +2,20 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
         int i = 0;
         int j = nums.length - 1;
+        while (i < j){
+            int mid = (i + j)/2;
+            if (mid % 2 == 1) mid--; //mid is even number
+            if (nums[mid] != nums[mid + 1]) j = mid;
+            else i = mid + 2;
+        }
+        return nums[i];
+    }
+}
+//Update
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
         
         if(nums[i] != nums[i+1]) return nums[0];
         if(nums[j] != nums[j-1]) return nums[j];
