bf63199a3b6f3b7316393b3ffc3b8f367215a5e2
Di Wang <jiankong3@gmail.com>
2018-07-04 08:48:26 -0700

    Update

--- 75. Sort Colors.java
+++ 75. Sort Colors.java
@@ -1,36 +1,21 @@
-//Simple One Pass
-public class Solution {
+class Solution {
     public void sortColors(int[] nums) {
-        //Counting Sort
-        int cnt_0 = 0;
-        int cnt_1 = 0;
-        int cnt_2 = 0;
-        
-        for(int n:nums){
-            if(n==0){
-                cnt_0 ++;
-            }
-            else if(n==1){
-                cnt_1 ++;
-            }
-            else if(n==2){
-                cnt_2 ++;
+        int i = 0; //after last 0
+        int j = 0; //after last 1
+        int k = nums.length - 1; //before first 2
+        while (j <= k){
+            if (nums[j] == 2){
+                swap(nums, j, k--); //Key 不能跳过
+            } else if (nums[j] == 1){
+                j++;
+            } else if (nums[j] == 0){
+                swap(nums, j++, i++);
             }
         }
-        
-        for(int i=0; i<nums.length; i++){
-            if(i< cnt_0){
-                nums[i] = 0;
-            }
-            else if(i>= cnt_0 && i< cnt_0 + cnt_1){
-                nums[i] = 1;
-            }
-            else{
-                nums[i] = 2;
-            }
-        }
-        
+    }
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
-
-//
