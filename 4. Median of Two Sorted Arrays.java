8f375d2c6b931f32159ad8aba72f90759ce3c874
Di Wang <jiankong3@gmail.com>
2018-07-06 07:02:45 -0700

    Update

--- 4. Median of Two Sorted Arrays.java
+++ 4. Median of Two Sorted Arrays.java
@@ -1,40 +1,25 @@
-public class Solution {
-    
+class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        if((m+n)%2 == 0) {
-            return (findkth(nums1, 0, nums2, 0, (m+n)/2) + findkth(nums1, 0, nums2, 0, (m+n)/2 + 1))/2.0;
+        if ((m + n) % 2 == 0){
+            return ((double)findKth(nums1, nums2, 0, 0, (m + n)/2) + 
+                (double)findKth(nums1, nums2, 0, 0, (m + n)/2 + 1))/2.0;
         }
-        return (double)findkth(nums1, 0, nums2, 0, (m+n)/2 + 1);
+        return (double)findKth(nums1, nums2, 0, 0, (m + n)/2 + 1);
     }
     
-    public int findkth(int[] nums1, int p, int[] nums2, int q, int k) {
-        if(p >= nums1.length){
-            return nums2[q + k -1];
-        } //num1 all gone
+    public int findKth(int[] nums1, int[] nums2, int a, int b, int k){
+        if (a == nums1.length) return nums2[b + k - 1];
+        if (b == nums2.length) return nums1[a + k - 1];
+        if (k == 1) return Math.min(nums1[a], nums2[b]);
         
-        if(q >= nums2.length){
-            return nums1[p + k -1];
-        }
-        
-        if(k == 1) return Math.min(nums1[p],nums2[q]);
-        
-        int val1 = Integer.MAX_VALUE, val2 = Integer.MAX_VALUE;
-        
-        //Bound for p + k/2 & q + k/2
-        if(p + k/2 - 1 < nums1.length){
-            val1 = nums1[p + k/2 - 1];
-        }
-        if(q + k/2 -1 < nums2.length){
-            val2 = nums2[q + k/2 - 1];
-        }
-        
-        if(val1 < val2){
-            return findkth(nums1, p + k/2, nums2, q, k - k/2);
-        }
-        else{
-            return findkth(nums1, p, nums2, q + k/2, k - k/2);
-        }
+        //第四个element, a, index = 0, b, index = 0, 找第二个
+        int valA = (a + k/2 - 1< nums1.length) ? nums1[a + k/2 - 1] : Integer.MAX_VALUE;
+        int valB = (b + k/2 - 1< nums2.length) ? nums2[b + k/2 - 1] : Integer.MAX_VALUE;
+
+        if (valA > valB){ //element in B can be removed
+            return findKth(nums1, nums2, a, b + k/2, k - k/2);
+        } return findKth(nums1, nums2, a + k/2, b, k - k/2);
     }
 }
