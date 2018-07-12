09e030430d0293135d48054140f501a50d7d7d5d
Di Wang <jiankong3@gmail.com>
2018-07-12 07:45:04 -0700

    Update

--- 127. Word Ladder.java
+++ 127. Word Ladder.java
@@ -1,3 +1,37 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int step = 1;
+        Set<String> visited = new HashSet<String>();
+        Queue<String> q = new LinkedList<String>();
+        q.offer(beginWord);
+        visited.add(beginWord);
+        while (!q.isEmpty()){
+            if (visited.contains(endWord)) break;
+            int size = q.size();
+            for (int i = 0; i < size; i++){
+                String cur = q.poll();
+                for (String s : wordList){
+                    if (!visited.contains(s) && distance(s, cur) == 1){
+                        q.offer(s);
+                        visited.add(s);
+                    }
+                }
+            }
+            step++;
+       }
+       return (visited.contains(endWord)) ? step : 0;
+    }
+    
+    public int distance(String a, String b){
+        //a.length() == b.length();
+        int count = 0;
+        for (int i = 0; i < a.length(); i++){
+            if (a.charAt(i) != b.charAt(i)) count++;
+        }
+        return count;
+    }
+}
+//
 public class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         
