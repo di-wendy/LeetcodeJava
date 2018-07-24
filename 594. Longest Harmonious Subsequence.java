f62c67d2660023e2e0a24ea0604c86b749138de1
Di Wang <jiankong3@gmail.com>
2018-07-23 22:02:38 -0700

    Map

---
+++ 594. Longest Harmonious Subsequence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) map.put(n, map.getOrDefault(n, 0) + 1);
+        int ans = 0;
+        for (int n : map.keySet()){
+            if (map.containsKey(n + 1)){
+                ans = Math.max(ans, map.get(n + 1) + map.get(n));
+            }
+        }
+        return ans;
+    }
+}
