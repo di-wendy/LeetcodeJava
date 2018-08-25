9d52d3b5e928c2a7672b539ecde3d33bc7e51457
Di Wang <jiankong3@gmail.com>
2018-08-25 15:07:36 -0700

    Trie Answer

--- 648. Replace Words.java
+++ 648. Replace Words.java
@@ -1,3 +1,58 @@
+class Node{
+    Node[] children;
+    boolean end;
+    String word;
+    public Node(){
+        this.children = new Node[26];
+        end = false;
+        word = "";
+    }
+}
+
+public class Solution {
+    public String replaceWords(List<String> dict, String sentence) {
+        Node root = new Node();
+        
+        //Build Trie
+        for (String s : dict){
+            Node point = root;
+            for (int i = 0; i < s.length(); i++){
+                char c = s.charAt(i);
+                if (point.children[c - 'a'] == null){
+                    point.children[c - 'a'] = new Node();
+                }
+                point = point.children[c - 'a'];
+                if (i == s.length() - 1) {
+                    point.end = true;
+                    point.word = s;
+                }
+            }
+        }
+        //Build answer
+        String[] ans = sentence.split(" ");
+        for (int i = 0; i < ans.length; i++){
+            Node point = root;
+            for (char c : ans[i].toCharArray()){
+                if (point.children[c - 'a'] == null){
+                    break;
+                } else {
+                    if (point.children[c - 'a'].end) {
+                        ans[i] = point.children[c - 'a'].word;
+                        break;
+                    }
+                    point = point.children[c - 'a'];
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ans.length; i++){
+            sb.append(ans[i]);
+            if (i != ans.length - 1) sb.append(" ");
+        }
+        return sb.toString();
+    }
+}
+//Search
 public class Solution {
     public String replaceWords(List<String> dict, String sentence) {
         
